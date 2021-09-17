0b7f2407794ecbbd1afe006b4b615d9124e4a704
Nico Rithner <nicorithner@gmail.com>
2021-09-17 13:04:01 -0600

    Lesson 23

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -43,3 +43,18 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // {x: 10, y: 20};
+
+// 2) When we declare a variable on one line and initialize it later
+/**
+ * In the example below we have the declaration in line 51
+ * and the initialization in line 56
+ */
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
