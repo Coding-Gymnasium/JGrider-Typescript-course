5a9836735c5bf40b3539ee5a6dd43d71e7126acf
Nico Rithner <nicorithner@gmail.com>
2021-09-17 15:02:49 -0600

    Lesson 31 & 32

--- features/arrays.ts
+++ features/arrays.ts
@@ -1,6 +1,18 @@
 const carMakers = ['ford', 'toyota', 'chevy'];
 
-const dates = [new Date(), new Date()];
+const dates = [new Date(), new Date()]; // TS assigned 'dates: Date[]' using inference.
 
 const carsByMake = [['f150'], ['corolla'], ['camaro']]; // carsByMake: string[][]
 // const carMakers: string[][] = []; // if the array was initialized empty.
+
+// Help with inference when extracting values.
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100); // Argument of type 'number' is not assignable to parameter of type 'string'
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
