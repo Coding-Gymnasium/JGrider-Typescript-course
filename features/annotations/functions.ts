c948dc929d5d9c8d0a94e1383058d1435d684284
Nico Rithner <nicorithner@gmail.com>
2021-09-17 14:24:17 -0600

    Lesson 28

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -13,3 +13,20 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  // use 'never' when there isn't a return in the function.
+  throw new Error(message);
+};
+
+const throwError1 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+
+  return message;
+};
