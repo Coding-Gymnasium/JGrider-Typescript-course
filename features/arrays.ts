d616f9c045e02bdae1d35a3a7185f7b9ab6e767f
Nico Rithner <nicorithner@gmail.com>
2021-09-17 14:52:21 -0600

    Lesson 30

---
+++ features/arrays.ts
@@ -0,0 +1,6 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+
+const dates = [new Date(), new Date()];
+
+const carsByMake = [['f150'], ['corolla'], ['camaro']]; // carsByMake: string[][]
+// const carMakers: string[][] = []; // if the array was initialized empty.
