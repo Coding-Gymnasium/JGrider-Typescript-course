6d7cf90000bdea239ae338d7a2f64581108c4692
Nico Rithner <nicorithner@gmail.com>
2021-09-17 09:47:26 -0600

    Lesson 14

---
+++ features/type.ts
@@ -0,0 +1,11 @@
+const today = new Date();
+today.getMonth();
+// today.ajfldajla // give us an error because the method doesn't exist
+
+const person = {
+  age: 20,
+};
+
+class Color {}
+
+const red = new Color();
