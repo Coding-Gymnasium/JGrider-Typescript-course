437e4fe4cfe22cd189e7f636a7e9f07358b81467
Nico Rithner <nicorithner@gmail.com>
2021-09-21 10:47:20 -0600

    Complete lesson 47

--- features/classes.ts
+++ features/classes.ts
@@ -1,19 +1,20 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
 class Car extends Vehicle {
-  honk(): void {
+  private drive(): void {
     console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
-car.honk();
-car.drive();
+// car.honk(); // 'honk()' can't be access outside the class Vehicle, except by it's children classes. 'startDrivingProcess' is a child class so it can use it. 
+car.startDrivingProcess();
