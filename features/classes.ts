2447691a90abd2d203521450905afafd3c949668
Nico Rithner <nicorithner@gmail.com>
2021-09-21 11:08:47 -0600

    Complete lesson 49 'Fields with inheritance'

--- features/classes.ts
+++ features/classes.ts
@@ -8,8 +8,11 @@ class Vehicle {
 const vehicle = new Vehicle('orange');
 console.log(vehicle.color);
 
-/*
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
   private drive(): void {
     console.log('vroom');
   }
@@ -20,7 +23,6 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, 'red');
 // car.honk(); // 'honk()' can't be access outside the class Vehicle, except by it's children classes. 'startDrivingProcess' is a child class so it can use it.
 car.startDrivingProcess();
-*/
