01fec85863b4486ba78913b6909d5c39d9604655
Nico Rithner <nicorithner@gmail.com>
2021-09-22 11:09:56 -0600

    Complete lesson 71. Add .prettierrc

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,11 +1,7 @@
-import { User } from './User';
-import { Company } from './Company';
-
 /**
  * Instructions to every other class
  * on how to be an argument to 'addMarker'
  */
-
 interface Mappable {
   location: {
     lat: number;
@@ -17,19 +13,28 @@ export class CustomMap {
   private googleMap: google.maps.Map;
 
   constructor(divId: string) {
-    this.googleMap = new google.maps.Map(document.getElementById(divId), {
-      zoom: 1,
-      center: { lat: 0, lng: 0 },
-    });
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId),
+      {
+        zoom: 1,
+        center: { lat: 0, lng: 0 },
+      },
+    );
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there',
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
