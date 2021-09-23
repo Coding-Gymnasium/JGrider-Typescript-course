8081d7975c11ca6f3157ed53a4476e75ebcb43c0
Nico Rithner <nico@altvia.com>
2021-09-23 11:04:12 -0600

    Complete lesson 85
    Co-authored-by: Nico nicorithner@gmail.com

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,27 +1,6 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                if (typeof this.collection === 'string') {
-                    // logic goes here
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var sorter = new Sorter_1.Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
