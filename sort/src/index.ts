274fd1d6f0892da5b0c20bce32461c58f77695e5
Nico Rithner <nicorithner@gmail.com>
2021-09-24 16:02:40 -0600

    Complete lesson 97

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -3,17 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-/*
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// numbersCollection.sort();
+// console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter1 = new Sorter(charactersCollection);
-sorter1.sort();
-console.log(charactersCollection.data);
-*/
+// const charactersCollection = new CharactersCollection('Xaayb');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -21,6 +17,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
