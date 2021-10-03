960a61f8b4285fa3a05cd26a6e208b09eaed7f6b
Nico Rithner <nicorithner@gmail.com>
2021-10-03 08:54:29 -0600

    Complete lesson 132

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,15 +3,13 @@ import { MatchReader } from './MatchReader';
 import { ConsoleReport } from './reportTargets/ConsoleReports';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
